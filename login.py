b0afa18fede802d1c8c423ab7c857fe46b46a31c
magwit <magnushakon@hotmail.com>
2024-11-16 17:34:30 +0200

    Test login

---
+++ login.py
@@ -0,0 +1,30 @@
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False, slow_mo=2500)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://symonstorozhenko.wixsite.com/website-1")
+    # page.pause()
+    page.wait_for_load_state("networkidle")
+    page.get_by_role("button", name="Log In").click()
+    page.wait_for_load_state("networkidle")
+    page.get_by_test_id("signUp.switchToSignUp").click()
+    page.get_by_role("button", name="Log in with Email").click()
+    page.get_by_test_id("emailAuth").get_by_label("Email").fill(
+        "bob.dobbs@fakemail.com"
+    )
+    page.get_by_label("Password").click()
+    page.get_by_label("Password").fill("byxa111")
+    page.get_by_label("Password").press("Enter")
+    page.wait_for_load_state("networkidle")
+    expect(page.get_by_role("button", name="Log In")).to_be_hidden(timeout=6666)
+    
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
